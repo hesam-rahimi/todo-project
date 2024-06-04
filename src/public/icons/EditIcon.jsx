ae7ebb82dad6b829d97aa4c9dddd0266969ff847
hesam <hesam82rhm@gmail.com>
2024-06-04 23:32:32 +0330

    add edit and trash icon

---
+++ src/public/icons/EditIcon.jsx
@@ -0,0 +1,14 @@
+function EditIcon({ className }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={`${className} stroke-[#CDCDCD]`} fill="none" viewBox="0 0 18 18">
+      <path
+        // stroke="#CDCDCD"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.673 5.991L2 12.664V16h3.336l6.673-6.673M8.673 5.991l2.392-2.393.002-.001c.33-.33.494-.494.684-.556a.834.834 0 01.516 0c.19.062.355.226.684.555l1.45 1.451c.331.33.496.496.558.686a.834.834 0 010 .516c-.062.19-.227.355-.556.685h-.001l-2.393 2.393M8.673 5.991l3.336 3.336"
+      ></path>
+    </svg>
+  );
+}
+
+export default EditIcon;
